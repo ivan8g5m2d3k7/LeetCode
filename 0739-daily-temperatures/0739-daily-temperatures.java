06ab8cfccbbf27ef1b21742453b931ae16ae9755
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2024-10-22 09:21:42 +0300

    Time: 24 ms (87.38%), Space: 56 MB (98.54%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,31 +1,20 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-            int currMax = temperatures[temperatures.length - 1];
-            int[] res1 = new int[temperatures.length];
-            for (int i = temperatures.length - 2; i >= 0; i--) {
-                if (temperatures[i] >= currMax) {
-                    res1[i] = 0;
-                    currMax = temperatures[i];
+            Deque<Integer> stack = new ArrayDeque<>();
+            int[] res = new int[temperatures.length];
+            for (int i = 0; i < temperatures.length; i++) {
+                if (stack.isEmpty() || temperatures[i] <= temperatures[stack.peek()]) {
+                    stack.push(i);
                 } else {
-                    if (temperatures[i] < temperatures[i + 1]) {
-                        res1[i] = 1;
-                    } else if (temperatures[i] == temperatures[i + 1]) {
-                        res1[i] = res1[i + 1] + 1;
-                    } else {
-                        int j = i + 1;
-                        while (j < temperatures.length && temperatures[i] >= temperatures[j]) {
-                            if (res1[j] == 0) {
-                                j++;
-                            } else {
-                                j = j + res1[j];
-                            }
-                        }
-
-                        res1[i] = j - i;
+                    while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                        int prev = stack.pop();
+                        res[prev] = i - prev;
                     }
+
+                    stack.push(i);
                 }
             }
 
-            return res1;
+            return res;
         }
 }
