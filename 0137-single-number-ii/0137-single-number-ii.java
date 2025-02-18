011bb143154829ae11af2b6792d40dd108f145e0
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-02-18 08:19:07 +0200

    Time: 0 ms (100%), Space: 45.9 MB (12.25%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+            int one = 0;
+            int two = 0;
+            for (int n : nums) {
+                one = (one ^ n) & (~two);
+                two = (two ^ n) & (~one);
+            }
+            
+            return one;
+        }
+}
