507e861e67ddc9274214b1a7652e09324b8be92f
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-04-26 15:56:08 +0300

    Time: 29 ms (55.08%), Space: 55.8 MB (34.81%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,29 +1,80 @@
 class Solution {
-        public String replaceWords(List<String> dictionary, String sentence) {
-            String[] words = sentence.split("\\s");
-            String[] replacedWords = new String[words.length];
-            int i = 0;
-            for (String word : words) {
-                for (String root : dictionary) {
-                    if (word.startsWith(root)) {
-                        if (replacedWords[i] == null || replacedWords[i].length() > root.length()) {
-                            replacedWords[i] = root;
+
+        class TrieNode {
+            private Character letter;
+            private TrieNode[] array = new TrieNode[26];
+            private boolean isWord;
+            
+            public TrieNode() {
+                
+            }
+            
+            public TrieNode(char c) {
+                this.letter = c;
+            }
+        }
+        
+        class Trie {
+            private TrieNode root;
+            
+            public Trie() {
+                this.root = new TrieNode('.');
+            }
+            
+            public boolean insert(String word) {
+                TrieNode[] array = this.root.array;
+                boolean flag = false;
+                for (int i = 0; i < word.length(); i++) {
+                    if (array[word.charAt(i) - 'a'] != null) {
+                        if (i == word.length() - 1) {
+                            array[word.charAt(i) - 'a'].isWord = true;
                         }
+                        array = array[word.charAt(i) - 'a'].array;
+                        continue;
+                    }
+                    
+                    flag = true;
+                    TrieNode node = new TrieNode(word.charAt(i));
+                    array[word.charAt(i) - 'a'] = node;
+                    array = array[word.charAt(i) - 'a'].array;
+                    
+                    if (i == word.length() - 1) {
+                        node.isWord = true;
                     }
                 }
                 
-                if (replacedWords[i] == null) {
-                    replacedWords[i] = word;
+                return flag;
+            }
+            
+            public String getShortestRoot(String word) {
+                TrieNode current = this.root;
+                for (int i = 0; i < word.length(); i++) {
+                    if (current.array[word.charAt(i) - 'a'] != null) {
+                        if (current.array[word.charAt(i) - 'a'].isWord) {
+                            return word.substring(0, i + 1);
+                        }
+                        
+                        current = current.array[word.charAt(i) - 'a'];
+                    } else {
+                        break;
+                    }
                 }
-                i++;
+                
+                return word;
             }
+        }
 
-            StringBuilder sb = new StringBuilder();
-            for (String str : replacedWords) {
-                sb.append(str).append(' ');
+        public String replaceWords(List<String> dictionary, String sentence) {
+            Trie trie = new Trie();
+            for (String root : dictionary) {
+                trie.insert(root);
             }
-            sb.deleteCharAt(sb.length() - 1);
-
-            return sb.toString();
+            
+            String[] words = sentence.split("\\s+");
+            for (int i = 0; i < words.length; i++) {
+                words[i] = trie.getShortestRoot(words[i]);
+            }
+            
+            return String.join(" ", words);
         }
     }
