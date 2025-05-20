7bb01c2ee3c612310a41e1db79662b41e04bab17
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-05-20 09:48:41 +0300

    Time: 3 ms (22.27%), Space: 41.7 MB (49.63%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,47 +1,24 @@
-public class Solution {
-    public int mincostTickets(int[] days, int[] costs) {
-        // dp[i] = min cost to cover all travel days starting from days[i]
-        Integer[] memo = new Integer[days.length];
-        return dp(0, days, costs, memo);
+class Solution {
+        public int mincostTickets(int[] days, int[] costs) {
+            // dp[i] = min cost to travel up to day i, inclusive
+            int[] dp = new int[366];
+            dp[days[0]] = costs[0];
+            Set<Integer> set = new HashSet<>(days.length);
+            for (int d : days) {
+                set.add(d);
+            }
+
+            for (int i = 1; i <= days[days.length - 1]; i++) {
+                if (set.contains(i)) {
+                    int one = dp[i - 1] + costs[0];
+                    int seven = dp[Math.max(0, i - 7)] + costs[1];
+                    int thirty = dp[Math.max(0, i - 30)] + costs[2];
+                    dp[i] = Math.min(one, Math.min(seven, thirty));
+                } else {
+                    dp[i] = dp[i - 1];
+                }
+            }
+            
+            return dp[days[days.length - 1]];
+        }
     }
-
-    private int dp(int i, int[] days, int[] costs, Integer[] memo) {
-        // Base case: no more days to cover
-        if (i >= days.length) {
-            return 0;
-        }
-
-        // Memoization check
-        if (memo[i] != null) {
-            return memo[i];
-        }
-
-        int n = days.length;
-
-        // Option 1: Buy 1-day pass
-        int j = i;
-        while (j < n && days[j] < days[i] + 1) {
-            j++;
-        }
-        int cost1 = costs[0] + dp(j, days, costs, memo);
-
-        // Option 2: Buy 7-day pass
-        j = i;
-        while (j < n && days[j] < days[i] + 7) {
-            j++;
-        }
-        int cost7 = costs[1] + dp(j, days, costs, memo);
-
-        // Option 3: Buy 30-day pass
-        j = i;
-        while (j < n && days[j] < days[i] + 30) {
-            j++;
-        }
-        int cost30 = costs[2] + dp(j, days, costs, memo);
-
-        // Take the minimum of the three options
-        int minCost = Math.min(cost1, Math.min(cost7, cost30));
-        memo[i] = minCost;
-        return minCost;
-    }
-}
