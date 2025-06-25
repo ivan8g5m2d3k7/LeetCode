cff61975c3b0593e99f94c9f78a4d27eb7900a44
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-06-25 11:21:29 +0300

    Time: 36 ms (14.31%), Space: 45 MB (19.92%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -20,6 +20,13 @@ class Solution {
                         } else {
                             right--;
                         }
+                        
+                        while (left > j + 1 && right < n - 1 && left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left > j + 1 && right < n - 1 && left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
                     }
                 }
             }
