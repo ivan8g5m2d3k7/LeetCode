3c45db3d1af62e37fcb542d1d107bb5f8b01ed2c
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-07-15 05:46:09 -0700

    Time: 1 ms (29.58%), Space: 44.2 MB (93.96%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,45 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
         public int kthSmallest(TreeNode root, int k) {
-            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, (a, b) -> Integer.compare(b, a));
-            Deque<TreeNode> queue = new ArrayDeque<>();
-            queue.offer(root);
+            int counter = 0;
+            Deque<TreeNode> stack = new ArrayDeque<>();
+            stack.offer(root);
+            Set<TreeNode> visited = new HashSet<>();
+            visited.add(root);
 
-            while (!queue.isEmpty()) {
-                int size = queue.size();
-                for (int i = 0; i < size; i++) {
-                    TreeNode curr = queue.poll();
-                    int val = curr.val;
-                    if (maxHeap.size() < k) {
-                        maxHeap.offer(val);
-                    } else if (!maxHeap.isEmpty() && val < maxHeap.peek()) {
-                        maxHeap.poll();
-                        maxHeap.offer(val);
-                    }
-
-                    if (curr.left != null) {
-                        queue.offer(curr.left);
-                    }
-                    if (curr.right != null) {
-                        queue.offer(curr.right);
-                    }
+            while (!stack.isEmpty()) {
+                TreeNode current = stack.peek();
+                if (current.left != null && !visited.contains(current.left)) {
+                    stack.push(current.left);
+                    visited.add(current.left);
+                    continue;
+                }
+                
+                current = stack.pop();
+                if (++counter == k) {
+                    return current.val;
+                }
+                
+                if (current.right != null && !visited.contains(current.right)) {
+                    stack.push(current.right);
                 }
             }
-
-            return maxHeap.peek();
+            
+            return -1;
         }
     }
