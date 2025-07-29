67c06d0898c8c6f06e9f4a1d2ac7da0bfc80df89
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-07-29 01:29:24 -0700

    Time: 2 ms (92.66%), Space: 47.4 MB (26.55%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted-2.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted-2.java
@@ -1,34 +1,17 @@
 class Solution {
         public int[] twoSum(int[] numbers, int target) {
-            for (int i = 0; i < numbers.length; i++) {
-                if (i > 0 && numbers[i] == numbers[i - 1]) {
-                    continue;
-                }
-
-                int val = target - numbers[i];
-                int left = 0;
-                int right = numbers.length - 1;
-
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
-
-                    if (numbers[mid] == val) {
-                        if (mid < i) {
-                            return new int[] {mid + 1, i + 1};
-                        } else if (mid > i) {
-                            return new int[] {i + 1, mid + 1};
-                        } else {
-                            if (left != mid && numbers[left] == val) {
-                                return left < i ? new int[] {left + 1, i + 1} : new int[] {i + 1, left + 1};
-                            } else {
-                                left++;
-                            }
-                        }
-                    } else if (numbers[mid] < val) {
-                        left = mid + 1;
-                    } else if (numbers[mid] > val) {
-                        right = mid - 1;
-                    }
+            int left = 0;
+            int right = numbers.length - 1;
+            
+            while (left < right) {
+                int sum = numbers[left] + numbers[right];
+                
+                if (sum == target) {
+                    return new int[] {left + 1, right + 1};
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
             
