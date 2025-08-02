3058ea99df1d8b3de79e5e340bcc86f1177d7d40
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-08-02 00:48:52 -0700

    Medium 0137-single-number-ii 0 ms (100%) 45.4 MB (79.8%)

---
+++ 0137-single-number-ii/0137-single-number-ii-08-02-2025-10-48-51.java
@@ -0,0 +1,12 @@
+class Solution {
+        public int singleNumber(int[] nums) {
+            int seenOnce = 0;
+            int seenTwice = 0;
+            for (int num : nums) {
+                seenOnce = (seenOnce ^ num) & (~seenTwice);
+                seenTwice = (seenTwice ^ num) & (~seenOnce);
+            }
+            
+            return seenOnce;
+        }
+    }
