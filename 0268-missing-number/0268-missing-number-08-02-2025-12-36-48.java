876a04c25d78ef6cda6d82e106ab11962d2332f6
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-08-02 02:36:49 -0700

    Easy 0268-missing-number 0 ms (100%) 45.5 MB (42.5%)

---
+++ 0268-missing-number/0268-missing-number-08-02-2025-12-36-48.java
@@ -0,0 +1,12 @@
+class Solution {
+        public int missingNumber(int[] nums) {
+            int xor = 0;
+            for (int i = 0; i < nums.length; i++) {
+                xor ^= i ^ nums[i];
+            }
+            
+            xor ^= nums.length;
+            
+            return xor;
+        }
+    }
