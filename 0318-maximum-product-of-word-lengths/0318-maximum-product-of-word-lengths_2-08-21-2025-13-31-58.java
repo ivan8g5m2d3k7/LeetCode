fe640097fc3a369cceb25f9e25e5716d7c94edba
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-08-21 03:31:59 -0700

    Medium 0318-maximum-product-of-word-lengths 1033 ms (11.2%) 46 MB (85.26%)

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths_2-08-21-2025-13-31-58.java
@@ -0,0 +1,32 @@
+ class Solution {
+        public int maxProduct(String[] words) {
+            int max = 0;
+            for (int i = 0; i < words.length - 1; i++) {
+                int first = 0;
+                for (int j = 0; j < words[i].length(); j++) {
+                    first |= (1 << (words[i].charAt(j) - 97));
+                }
+
+                for (int k = i + 1; k < words.length; k++) {
+                    int second = 0;
+                    for (int j = 0; j < words[k].length(); j++) {
+                        second |= (1 << (words[k].charAt(j) - 97));
+                    }
+
+                    boolean skip = false;
+                    for (int b = 0; b < 26; b++) {
+                        if ( ( (first >> b) & (second >> b) ) == 1) {
+                            skip = true;
+                            break;
+                        }
+                    }
+
+                    if (!skip) {
+                        max = Math.max(max, words[i].length() * words[k].length());
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
