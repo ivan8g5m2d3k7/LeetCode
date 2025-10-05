33ebe6f28e4978707e9390739e9f075aed0b31c0
ivan8g5m2d3k7 <ivan8g5m2d3k7@gmail.com>
2025-10-05 08:44:12 +0300

    Medium 0139-word-break 2 ms (85.23%) 41.5 MB (98.71%)

---
+++ 0139-word-break/0139-word-break-10-05-2025-08-44-11.java
@@ -0,0 +1,17 @@
+class Solution {
+        public boolean wordBreak(String s, List<String> wordDict) {
+            boolean[] dp = new boolean[s.length() + 1];
+            dp[0] = true;
+            for (int i = 1; i <= s.length(); i++) {
+                for (String word : wordDict) {
+                    if (i >= word.length() && s.startsWith(word, i - word.length())
+                    && dp[i - word.length()]) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[s.length()];
+        }
+    }
